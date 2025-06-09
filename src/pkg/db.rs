de0e7883e9f10b022b9579ef1544f37ec228a440
kardo <kardotinambunan12@gmail.com>
2025-06-09 13:26:17 +0700

    update api login

--- src/pkg/db.rs
+++ src/pkg/db.rs
@@ -6,8 +6,8 @@ pub fn db_connection() -> PooledConn {
     dotenv().ok();
     println!("DATABASE_URL from env: {:?}", env::var("DATABASE_URL"));
 
-    // let url = env::var("DATABASE_URL").expect("DATABASE_URL not set in .env");
-    let url = "mysql://root@202.74.74.133:3306/todo_list".to_string();
+    let url = env::var("DATABASE_URL").expect("DATABASE_URL not set in .env");
+    // let url = "mysql://root@202.74.74.133:3306/todo_list".to_string();
 
 
     let opts = Opts::from_url(&url).expect("Invalid database URL");
